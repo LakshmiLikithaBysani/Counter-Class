2298c168234581a5ead5b003998f690090ae3972
LakshmiLikithaBysani <162402980+LakshmiLikithaBysani@users.noreply.github.com>
2026-09-24 21:10:25 +0530

    Create CounterClass.js

---
+++ CounterClass.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+class CounterClass extends Component { 
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Counter: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+
+export default CounterClass;
